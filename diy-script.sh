69b65108b1781872bc7c49af12d91e257119e548
issaccv <17275050+issaccv@users.noreply.github.com>
2026-04-30 09:55:50 +0800

    try add sing-box

--- diy-script.sh
+++ diy-script.sh
@@ -122,5 +122,9 @@ find package/luci-theme-*/* -type f -name '*luci-theme-*' -print -exec sed -i '/
 # sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-v2ray-server/luasrc/model/cbi/v2ray_server/*.lua
 # sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-v2ray-server/luasrc/view/v2ray_server/*.htm
 
+# 添加其他源
+
+echo "src-git momo https://github.com/nikkinikki-org/OpenWrt-momo.git;main" >> "feeds.conf.default"
+
 ./scripts/feeds update -a
 ./scripts/feeds install -a
